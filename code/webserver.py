3844508bd6868a9f24f76d209fc880b246a4e27b
stefan <stefan@04d82e34-2818-47c0-b8a7-c090d6fd0e6d>
2012-05-29 10:29:01 +0000

    fork and privilege separation

--- code/webserver.py
+++ code/webserver.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-from os import path, curdir, sep
+import os
+from os import path, sep, stat
+from multiprocessing import Process
 
-# privilege separation
-# next meeting end of may
 class MyHandler(BaseHTTPRequestHandler):
 
-	documentroot = '/home/stefan/python/docs'
+	documentroot = '/home/stefan/sws/docs'
 
-	def do_GET(self):
+	def prochandler(self,resource):
+
 		try:
-			resource = path.abspath(self.documentroot + sep + self.path)
-			if resource.startswith(self.documentroot):
-				f = open (resource)
-				self.send_response(200)
-				self.send_header('Content-type','text/html')
-				self.end_headers()
-				self.wfile.write(f.read())
-				f.close()
-			else:
-				self.send_error(403,'Forbidden %s' % self.path)
-			return
+			print 'Process', os.getpid(), ', Uid:',os.getuid(),', Gid:',os.getgid()
+			st = os.stat(resource)
+			os.setgid(st.st_gid)
+			os.setuid(st.st_uid)
+			print 'Process', os.getpid(), ', Uid:',os.getuid(),', Gid:',os.getgid()
+			f = open (resource)
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write(f.read())
+			f.close()
 		except IOError:
 			self.send_error(404,'File Not Found %s' % self.path)
 
-	
+
+	def do_GET(self):
+		resource = path.abspath(self.documentroot + sep + self.path)
+		if resource.startswith(self.documentroot):			
+			p = Process(target=self.prochandler, args=(resource,))
+			p.start()
+			p.join()
+		else:
+			self.send_error(403,'Forbidden %s' % self.path)
+		return
+
+
 
 def main():
 	try:
-		server = HTTPServer(('',85), MyHandler)
+		server = HTTPServer(('',80), MyHandler)
 		print 'Server started'
 		server.serve_forever()
 	except KeyboardInterrupt:
