25f353c297180bd84286c27934ba594b7cbd4e8f
stefan <stefan@04d82e34-2818-47c0-b8a7-c090d6fd0e6d>
2012-06-16 16:01:18 +0000

    renaming of files

--- code/sws_listenerF_rootF.py
+++ code/sws_listenerF_rootF.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
 
-import select
 import socket
 import os
 from os import path, sep, stat
 from multiprocessing import Process
 import subprocess
 import re
-import signal
 import sys
+from time import gmtime, strftime
+# not python standard lib - for mime type detection
+import magic
 
-# what should happen if a file is owned by root? what if a cgi script? -> if script: either error or nobody user
+
 # don't always fork root process -> just if request headers have no content-length and if requested ressource is no cgi script and not a large file
 # test HTTP parser with many browser (ie, ff, opera, chrome, safari, lynx, wget)
+
+# GET request parameter parsing
+# POST request, parse body
+# CGI script execution: set environment variables according to RFC
+
+# transfer-encoding????
+
 
 class RequestProcessor:
 
@@ -39,32 +47,51 @@ class RequestProcessor:
 					else:
 						self.request.document = self.getFileContent()
 
-					# temporary assume html content type
-					self.request.responseHeaders['Content-Type'] = 'text/html'
-
-				except:					
+				except Exception:
 					self.request.setResponseError(500,'Internal Server Error','500: Internal Server Error')
 			else:
 				self.request.setResponseError(404,'File Not Found','404: The file could not be found')
 		else:
 			self.request.setResponseError(403,'Forbidden','403: Forbidden')
-		
 
 	def getFileContent(self):
-		print 'resource retrieval by:',os.getuid()
 		f = open(self.resource)
-		return f.read()
+		content = f.read()
+		f.close()
+		return content
 
 	def executeCGI(self):
-		print 'cgi execution by:',os.getuid()
-        	if os.access(self.resource, os.X_OK):
+		# check whether resource is an executable file
+        	if not os.access(self.resource, os.X_OK):
+			raise IOError
+		# check if resource is not owned by root
+		elif os.getuid() == SecureWebServer.PRIVILEGED_UID:
+			raise Exception
+		else:
                 	p = subprocess.Popen([self.resource],stdout=subprocess.PIPE)
 			return p.communicate()[0]
-		else:
-			raise IOError
 
 
 class HttpRequest:
+
+	# keepalive not supported by the server
+	CONNECTION_TYPE = 'close'
+	HTTP_VERSION = '1.1'
+
+        def __init__ (self, connection):
+		# request fields
+		self.connection = connection
+		self.command = None
+		self.path = None
+		self.protocolVersion = 'HTTP/' + HttpRequest.HTTP_VERSION
+		self.headers = {}
+		self.requestMessage = ''
+		# response fields
+		self.responseMessage = ''
+		self.statusCode = None
+		self.statusMessage = None
+		self.document = None
+		self.responseHeaders = {}
 
 	def receive(self):
 		data = ''
@@ -99,6 +126,7 @@ class HttpRequest:
 			else:
 				if (line == ''):
 					break
+
 				# header line
 				pos = line.find(':')
 				if pos <= 0 or pos >= len(line)-1:
@@ -107,39 +135,66 @@ class HttpRequest:
 				key = line[0:pos].strip()
 				value = line[pos+1:len(line)].strip()
 				self.headers[key] = value
-	
+
+
+		# check if POST message has a message body
+		entityBody = ''
+		if self.command == 'POST':
+			for key in self.headers.keys():
+				if key.lower() == 'content-length' and self.headers[key] != 0:
+					data = ''
+		        	        # get request body
+					#while not entityBody.endswith('\r\n\r\n'):
+					#	data = self.connection.recv(4096)
+					#	entityBody = entityBody + data
+					break
+
 		self.statusCode = 200
 		self.statusMessage = 'OK'
 		self.document = ''
-		self.requestMessage = msg
+		self.requestMessage = msg + entityBody
+
 
 	def setResponseError(self, errorCode, errorMessage, errorDocument):
 		self.statusCode = errorCode
 		self.statusMessage = errorMessage
 		self.document = errorDocument
-		self.responseHeaders['Content-Type'] = 'text/html'
 
-        def __init__ (self, connection):
-		# request fields
-		self.connection = connection
-		self.command = None
-		self.path = None
-		self.protocolVersion = 'HTTP/1.1'
-		self.headers = {}
-		self.requestMessage = ''
-		# response fields
-		self.responseMessage = ''
-		self.statusCode = None
-		self.statusMessage = None
-		self.document = None
-		self.responseHeaders = {}
-	
+
+	def getContentType(self):
+		mime = magic.Magic(mime=True)
+		mime_encoding = magic.Magic(mime_encoding=True)
+		contentType = mime.from_buffer(self.document)
+		charset = mime_encoding.from_buffer(self.document)
+		if charset != 'binary':
+			return contentType + ';charset=' + charset
+		else:
+			return contentType
+
+	def generateResponseHeaders(self):
+		self.responseHeaders['Connection'] = HttpRequest.CONNECTION_TYPE
+		self.responseHeaders['Content-Length'] = str(len(self.document))
+		self.responseHeaders['Content-Type'] = self.getContentType()
+		self.responseHeaders['Date'] = strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+		self.responseHeaders['Server'] = 'SWS/'+ str(SecureWebServer.VERSION)
+
 	def generateResponseMessage(self):
+		# generate response headers
+		self.generateResponseHeaders()
+
+		# generate Status line
 		m = self.protocolVersion+' '+str(self.statusCode)+' '+self.statusMessage+'\r\n'
+
 		# add headers
 		for hKey in self.responseHeaders.keys():
 			m = m + hKey + ':' + self.responseHeaders[hKey]+'\r\n'
-		self.responseMessage = m+'\r\n'+self.document
+
+		self.responseMessage = m + '\r\n'
+
+		# HEAD request must not have a response body
+		if self.command != 'HEAD':
+			self.responseMessage = self.responseMessage + self.document
+
 
 	def checkValidity(self):
 		if self.statusCode == 200:
@@ -181,7 +236,6 @@ class PrivilegedProcess:
 	def __init__ (self, webserver):
 		# forked root process does not need a open server/listening socket
 		webserver.listener.close()
-		print 'privileged process created: UID:', os.getuid()
 
 		# create UNIX socket for communication with client processes
 		rootListener = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
@@ -202,39 +256,46 @@ class PrivilegedProcess:
 			conn.close()
 
 
-class SecureWebServer:
+class ClientProcess:
 
-	def clientProcessHandler(self, connection):
-		print 'client communicating to server'
+	def __init__(self, webserver, connection, addr):
+		# forked client process does not need a open server/listening socket
+		webserver.listener.close()
+		print 'new client',addr
+		
 		# initialize HTTP Request
 		request = HttpRequest(connection)
 		# receive request data and parse it
 		request.receive()
 		
-		print 'unprivileged process received request - pid:',os.getpid()
-
 		# just forward request to root process if syntax is valid
 		if request.checkValidity():
-			print 'valid request forwarded to root'
 			# establish connection to root process
 			rootSocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-			rootSocket.connect(self.unixSocketPath)
+			rootSocket.connect(webserver.unixSocketPath)
 			# forward message to root process
 			rootSocket.send(request.requestMessage)
 			# wait for roots response
-			request.responseMessage = rootSocket.recv(4096)
+			responseMsg = ''
+			data = 'init'
+			while data != '':
+				data = rootSocket.recv(4096)
+				responseMsg = responseMsg + data
+			request.responseMessage = responseMsg
 			# close connection to root
 			rootSocket.close()
 		else:
-			print 'invalid request'
 			# error in request
 			request.generateResponseMessage()
 
 		# return response to client and close connection
 		request.sendResponse()
-		# remove client from socketlist
-		self.socketList.remove(connection)
 
+
+class SecureWebServer:
+
+	VERSION = 0.1
+	PRIVILEGED_UID = 0
 
 	def __init__(self, listenerUid, listenerGid, port=80, host='', unixSocketPath="/tmp/sws.peerweb.it"):
 		self.host = host
@@ -256,28 +317,18 @@ class SecureWebServer:
 		os.setgid(self.listenerGid)
 		os.setuid(self.listenerUid)
 
-		print 'listener serves forever: UID:', os.getuid()
-
-		self.socketList = [self.listener]
-
 		# serve forever
 		while 1:
-			try:
-				print self.socketList
-				readReady, writeReady, exceptReady = select.select(self.socketList, [], [])
-			except:
-				break;
+			# blocks until new incoming connection
+			conn, addr = self.listener.accept()
 
-			for readySocket in readReady:
-				if readySocket == self.listener:
-					conn, addr = self.listener.accept()
-					print 'new connection from',addr
-					self.socketList.append(conn)
-				else:
-					self.clientProcessHandler(readySocket)
-				
+			# create new client process for handling this connection
+			clientHandlerProcess = Process (target=ClientProcess,args=(self,conn,addr,))
+			clientHandlerProcess.start()
 
-		self.shutdown()
+			# listener process does not need to keep open the client connection
+			conn.close()
+
 
 	def shutdown (self):
 		self.listener.close()
--- code/sws_listenerS_v1_rootF.py
+++ code/sws_listenerS_v1_rootF.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import select
 import socket
 import os
 from os import path, sep, stat
@@ -7,8 +8,20 @@ from multiprocessing import Process
 import subprocess
 import re
 import sys
+from time import gmtime, strftime
+# not python standard lib - for mime type detection
+import magic
 
-# what should happen if a file is owned by root? what if a cgi script?
+
+# don't always fork root process -> just if request headers have no content-length and if requested ressource is no cgi script and not a large file
+# test HTTP parser with many browser (ie, ff, opera, chrome, safari, lynx, wget)
+
+# GET request parameter parsing
+# POST request, parse body
+# CGI script execution: set environment variables according to RFC
+
+# transfer-encoding????
+
 
 class RequestProcessor:
 
@@ -35,32 +48,51 @@ class RequestProcessor:
 					else:
 						self.request.document = self.getFileContent()
 
-					# temporary assume html content type
-					self.request.responseHeaders['Content-Type'] = 'text/html'
-
-				except:					
+				except Exception:
 					self.request.setResponseError(500,'Internal Server Error','500: Internal Server Error')
 			else:
 				self.request.setResponseError(404,'File Not Found','404: The file could not be found')
 		else:
 			self.request.setResponseError(403,'Forbidden','403: Forbidden')
-		
 
 	def getFileContent(self):
-		print 'resource retrieval by:',os.getuid()
 		f = open(self.resource)
-		return f.read()
+		content = f.read()
+		f.close()
+		return content
 
 	def executeCGI(self):
-		print 'cgi execution by:',os.getuid()
-        	if os.access(self.resource, os.X_OK):
+		# check whether resource is an executable file
+        	if not os.access(self.resource, os.X_OK):
+			raise IOError
+		# check if resource is not owned by root
+		elif os.getuid() == SecureWebServer.PRIVILEGED_UID:
+			raise Exception
+		else:
                 	p = subprocess.Popen([self.resource],stdout=subprocess.PIPE)
 			return p.communicate()[0]
-		else:
-			raise IOError
 
 
 class HttpRequest:
+
+	# keepalive not supported by the server
+	CONNECTION_TYPE = 'close'
+	HTTP_VERSION = '1.1'
+
+        def __init__ (self, connection):
+		# request fields
+		self.connection = connection
+		self.command = None
+		self.path = None
+		self.protocolVersion = 'HTTP/' + HttpRequest.HTTP_VERSION
+		self.headers = {}
+		self.requestMessage = ''
+		# response fields
+		self.responseMessage = ''
+		self.statusCode = None
+		self.statusMessage = None
+		self.document = None
+		self.responseHeaders = {}
 
 	def receive(self):
 		data = ''
@@ -95,6 +127,7 @@ class HttpRequest:
 			else:
 				if (line == ''):
 					break
+
 				# header line
 				pos = line.find(':')
 				if pos <= 0 or pos >= len(line)-1:
@@ -103,39 +136,66 @@ class HttpRequest:
 				key = line[0:pos].strip()
 				value = line[pos+1:len(line)].strip()
 				self.headers[key] = value
-	
+
+
+		# check if POST message has a message body
+		entityBody = ''
+		if self.command == 'POST':
+			for key in self.headers.keys():
+				if key.lower() == 'content-length' and self.headers[key] != 0:
+					data = ''
+		        	        # get request body
+					#while not entityBody.endswith('\r\n\r\n'):
+					#	data = self.connection.recv(4096)
+					#	entityBody = entityBody + data
+					break
+
 		self.statusCode = 200
 		self.statusMessage = 'OK'
 		self.document = ''
-		self.requestMessage = msg
+		self.requestMessage = msg + entityBody
+
 
 	def setResponseError(self, errorCode, errorMessage, errorDocument):
 		self.statusCode = errorCode
 		self.statusMessage = errorMessage
 		self.document = errorDocument
-		self.responseHeaders['Content-Type'] = 'text/html'
 
-        def __init__ (self, connection):
-		# request fields
-		self.connection = connection
-		self.command = None
-		self.path = None
-		self.protocolVersion = 'HTTP/1.1'
-		self.headers = {}
-		self.requestMessage = ''
-		# response fields
-		self.responseMessage = ''
-		self.statusCode = None
-		self.statusMessage = None
-		self.document = None
-		self.responseHeaders = {}
-	
+
+	def getContentType(self):
+		mime = magic.Magic(mime=True)
+		mime_encoding = magic.Magic(mime_encoding=True)
+		contentType = mime.from_buffer(self.document)
+		charset = mime_encoding.from_buffer(self.document)
+		if charset != 'binary':
+			return contentType + ';charset=' + charset
+		else:
+			return contentType
+
+	def generateResponseHeaders(self):
+		self.responseHeaders['Connection'] = HttpRequest.CONNECTION_TYPE
+		self.responseHeaders['Content-Length'] = str(len(self.document))
+		self.responseHeaders['Content-Type'] = self.getContentType()
+		self.responseHeaders['Date'] = strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+		self.responseHeaders['Server'] = 'SWS/'+ str(SecureWebServer.VERSION)
+
 	def generateResponseMessage(self):
+		# generate response headers
+		self.generateResponseHeaders()
+
+		# generate Status line
 		m = self.protocolVersion+' '+str(self.statusCode)+' '+self.statusMessage+'\r\n'
+
 		# add headers
 		for hKey in self.responseHeaders.keys():
 			m = m + hKey + ':' + self.responseHeaders[hKey]+'\r\n'
-		self.responseMessage = m+'\r\n'+self.document
+
+		self.responseMessage = m + '\r\n'
+
+		# HEAD request must not have a response body
+		if self.command != 'HEAD':
+			self.responseMessage = self.responseMessage + self.document
+
 
 	def checkValidity(self):
 		if self.statusCode == 200:
@@ -147,9 +207,9 @@ class HttpRequest:
 		self.connection.close()
 
 
-class SecureWebServer:
-
-	def privilegedClientProcessHandler(self, connection, addr, rootSocket):
+class UnprivilegedProcess:
+	
+	def __init__(self, connection, rootSocket):
 		# forked client process does not need a open root listening socket
 		rootSocket.close()
 
@@ -172,61 +232,69 @@ class SecureWebServer:
 		request.sendResponse()
 
 
-	def rootProcessHandler(self, serversocket):
+class PrivilegedProcess:
+
+	def __init__ (self, webserver):
 		# forked root process does not need a open server/listening socket
-		serversocket.close()
-		print 'root process created: UID:', os.getuid()
+		webserver.listener.close()
 
 		# create UNIX socket for communication with client processes
 		rootListener = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
 		try:
 			# remove eventual old socket
-			os.remove(self.unixSocketPath)
+			os.remove(webserver.unixSocketPath)
 		except OSError:
 			pass
-		rootListener.bind(self.unixSocketPath)
+		rootListener.bind(webserver.unixSocketPath)
 		# an unprivileged process has to have access to the socket
-		os.chown(self.unixSocketPath,self.listenerUid,self.listenerGid)
+		os.chown(webserver.unixSocketPath,webserver.listenerUid,webserver.listenerGid)
 		rootListener.listen(3)
 
 		while 1:
 			conn, addr = rootListener.accept()
-			privilegedClientProcess = Process (target=self.privilegedClientProcessHandler,args=(conn,addr,rootListener,))
-			privilegedClientProcess.start()
+			unprivilegedProcess = Process (target=UnprivilegedProcess,args=(conn,rootListener,))
+			unprivilegedProcess.start()
 			conn.close()
 
-	def clientProcessHandler(self, connection, addr, serversocket):
-		# forked client process does not need a open server/listening socket
-		serversocket.close()
-		print 'new client',addr
-		
+
+class ClientProcess:
+
+	def __init__(self, webserver, connection):
 		# initialize HTTP Request
 		request = HttpRequest(connection)
 		# receive request data and parse it
 		request.receive()
 		
-		print 'unprivileged process received request - pid:',os.getpid()
-
 		# just forward request to root process if syntax is valid
 		if request.checkValidity():
-			print 'valid request forwarded to root'
 			# establish connection to root process
 			rootSocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-			rootSocket.connect(self.unixSocketPath)
+			rootSocket.connect(webserver.unixSocketPath)
 			# forward message to root process
 			rootSocket.send(request.requestMessage)
 			# wait for roots response
-			request.responseMessage = rootSocket.recv(4096)
+			responseMsg = ''
+			data = 'init'
+			while data != '':
+				data = rootSocket.recv(4096)
+				responseMsg = responseMsg + data
+			request.responseMessage = responseMsg
 			# close connection to root
 			rootSocket.close()
 		else:
-			print 'invalid request'
 			# error in request
 			request.generateResponseMessage()
 
 		# return response to client and close connection
 		request.sendResponse()
+		# remove client from socketlist
+		webserver.socketList.remove(connection)
 
+
+class SecureWebServer:
+
+	VERSION = 0.1
+	PRIVILEGED_UID = 0
 
 	def __init__(self, listenerUid, listenerGid, port=80, host='', unixSocketPath="/tmp/sws.peerweb.it"):
 		self.host = host
@@ -239,28 +307,32 @@ class SecureWebServer:
 		self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.listener.bind((self.host, self.port))
 		self.listener.listen(5)
-		
+
 		# create root process which stays in background and forks child processes
-		rootProcess = Process (target=self.rootProcessHandler,args=(self.listener,))
+		rootProcess = Process (target=PrivilegedProcess,args=(self,))
 		rootProcess.start()
 
 		# remove root privilege from listener process
 		os.setgid(self.listenerGid)
 		os.setuid(self.listenerUid)
 
-		print 'listener serves forever: UID:', os.getuid()
+		self.socketList = [self.listener]
 
 		# serve forever
 		while 1:
-			# blocks until new incoming connection
-			conn, addr = self.listener.accept()
+			try:
+				readReady, writeReady, exceptReady = select.select(self.socketList, [], [])
+			except:
+				break;
 
-			# create new client process for handling this connection
-			clientHandlerProcess = Process (target=self.clientProcessHandler,args=(conn,addr,self.listener,))
-			clientHandlerProcess.start()
+			for readySocket in readReady:
+				if readySocket == self.listener:
+					conn, addr = self.listener.accept()
+					print 'new connection from',addr
+					self.socketList.append(conn)
+				else:
+					ClientProcess(self,readySocket)
 
-			# listener process does not need to keep open the client connection
-			conn.close()
 
 	def shutdown (self):
 		self.listener.close()
@@ -270,8 +342,5 @@ port = 80
 if len(sys.argv) > 1:
 	port = int(sys.argv[1])
 
-try:
-	s = SecureWebServer(1000,1000,port)
-except KeyboardInterrupt:
-	s.shutdown()
+SecureWebServer(1000,1000,port)
 
